75384b2a1fd925deeacb2e651be37c098454246f
Chris <chrishaining@yahoo.co.uk>
2020-01-18 14:38:19 +0000

    initial commit

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chrishaining/codeclan_python/practice/record_shop 5/record_store/.env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
